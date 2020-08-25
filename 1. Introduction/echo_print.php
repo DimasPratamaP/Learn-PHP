82c3a323edf5ecee7263fbb4d62f46d7b2f7917c
Dimas Pratama <54875915+dimpurw@users.noreply.github.com>
2020-08-25 08:15:08 +0700

    Add files via upload

---
+++ 1. Introduction/echo_print.php
@@ -0,0 +1,14 @@
+<?php 
+
+// output with echo
+echo "hello";
+echo '<br>';
+echo 'hai';
+
+//output with print
+print '<br>';
+print "hello";
+print "<br>";
+print 'hai';
+
+?>
---
+++ 1. Introduction/variabel.php
@@ -0,0 +1,11 @@
+<?php 
+
+//declaring variables
+$text = "Hello World";
+$number = 20;
+
+echo $text;
+echo "<br>";
+echo $number;
+
+?>
