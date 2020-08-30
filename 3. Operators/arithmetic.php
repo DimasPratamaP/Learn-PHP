ec3bf4f126afb193dfca1dd66b4f99d7813963f5
Dimas Pratama Purwadinata <54875915+dimaspratamap@users.noreply.github.com>
2020-08-30 19:55:59 +0700

    Add files via upload

---
+++ 3. Operators/arithmetic.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 6;
+$b = 8;
+
+echo ($a + $b);
+echo "<br>";
+echo ($a - $b);
+echo "<br>";
+echo ($a * $b);
+echo "<br>";
+echo ($a / $b);
+echo "<br>";
+echo ($a % $b);
+?>
---
+++ 3. Operators/assignment.php
@@ -0,0 +1,31 @@
+<?php
+$a = 5;
+echo $a;
+echo "<br>";
+
+$a = 8;
+$a += 5;
+echo $a;
+echo "<br>";
+
+$a = 10;
+$a -= 5;
+echo $a;
+echo "<br>";
+
+$a = 4;
+$a *= 4;
+echo $a;
+echo "<br>";
+
+$a = 15;
+$a /= 5;
+echo $a;
+echo "<br>";
+
+$a = 20;
+$a %= 5;
+echo $a;
+echo "<br>";
+
+?>
