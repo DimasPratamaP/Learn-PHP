eac2faef359e5e3552625f0800d1d18910dd02d2
Dimas Pratama Purwadinata <54875915+dimaspratamap@users.noreply.github.com>
2020-08-31 21:10:01 +0700

    Add files via upload

---
+++ 3. Operators/comparison.php
@@ -0,0 +1,29 @@
+<?php
+$a = 20;
+$b = 15;
+$c = "20";
+
+var_dump($a == $c);
+echo "<br>";
+
+var_dump($a === $c);
+echo "<br>";
+
+var_dump($a != $b);
+echo "<br>";
+
+var_dump($a !== $c);
+echo "<br>";
+
+var_dump($a < $b);
+echo "<br>";
+
+var_dump($a > $b);
+echo "<br>";
+
+var_dump($a <= $b);
+echo "<br>";
+
+var_dump($a >= $b);
+
+?>
---
+++ 3. Operators/incrementing_decrementing.php
@@ -0,0 +1,25 @@
+<?php
+$a = 5;
+echo++$a;
+echo "<br>";
+echo $a;
+echo "<hr>";
+
+$a = 5;
+echo$a++;
+echo "<br>";
+echo $a;
+echo "<hr>";
+
+$a = 5;
+echo--$a;
+echo "<br>";
+echo $a;
+echo "<hr>";
+
+$a = 5;
+echo$a--;
+echo "<br>";
+echo $a;
+
+?>
