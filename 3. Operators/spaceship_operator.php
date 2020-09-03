39eb4239a49739ea3e9eb30ea78c6b96a81e8ab8
Dimas Pratama Purwadinata <54875915+dimaspratamap@users.noreply.github.com>
2020-09-03 10:01:13 +0700

    Add files via upload

---
+++ 3. Operators/spaceship_operator.php
@@ -0,0 +1,25 @@
+<?php
+// comparing integers
+echo 1 <=> 1;
+echo "<br>";
+echo 1 <=> 2;
+echo "<br>";
+echo 2 <=> 1;
+echo "<hr>";
+
+// comparing floats
+echo 1.5 <=> 1.5;
+echo "<br>";
+echo 1.5 <=> 2.5;
+echo "<br>";
+echo 2.5 <=> 1.5;
+echo "<hr>";
+
+// comparing strings
+echo "a" <=> "a";
+echo "<br>";
+echo "a" <=> "b";
+echo "<br>";
+echo "b" <=> "a";
+
+?>
