c74951750bac15ab8cfbd582e87b96a59f32acd4
Dimas Pratama Purwadinata <54875915+DimasPratamaP@users.noreply.github.com>
2020-09-15 21:15:47 +0700

    Add files via upload

---
+++ 4. Flow Controls/switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$today = "Day";
+switch($today){
+    case "Mon";
+        echo "Monday";
+        break;
+    case "Tue";
+        echo "Tuesday";
+        break;
+    case "Sun";
+        echo "Sunday";
+        break;
+    default;
+        echo "No";
+        break;
+}
+?>
