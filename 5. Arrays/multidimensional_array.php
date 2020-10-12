a3482f9eaec4bf7a04dc79aa3753df625c1b1f79
Dimas Pratama Purwadinata <54875915+DimasPratamaP@users.noreply.github.com>
2020-10-12 16:38:02 +0700

    Add files via upload

---
+++ 5. Arrays/multidimensional_array.php
@@ -0,0 +1,17 @@
+<?php 
+$contact = array(
+	array(
+		"nama" => "Dimas",
+		"email" => "DimasPratamaPurwadinata@gmail.com",
+	),
+	array(
+		"nama" => "Pratama",
+		"email" => "PratamaPurwadinata@gmail.com",
+	),
+	array(
+		"nama" => "Purwadinata",
+		"email" => "Purwadinata@gmail.com",)
+	);
+
+echo "Dimas Pratama Email-id is: " .$contact[0]["email"];
+ ?>
